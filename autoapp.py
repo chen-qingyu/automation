e95327e7c831867bc5157a80f8c9185e3eaf60f5
chen-qingyu <chen_qingyu@qq.com>
2025-01-13 22:48:11 +0800

    refact(autoapp.py)

--- autoapp.py
+++ autoapp.py
@@ -17,11 +17,17 @@ from common import COLOR_START, COLOR_INFO, COLOR_FINISH, COLOR_ERROR
 def main():
     # check platform
     if platform.system() != 'Windows':
-        print(COLOR_ERROR + "This script currently only supports the Windows platform.\n")
+        print(COLOR_ERROR + "This script only supports Windows.\n")
         exit(-1)
 
+    help_text = """
+    install: install applications using `winget` or manually.
+    update:  update applications using `winget` or manually.
+    check:   check if applications are installed or available via `winget`.
+    """
+
     # parse command
-    parser = argparse.ArgumentParser(prog="autoapp", description="Python3 script for automating install applications.")
+    parser = argparse.ArgumentParser(prog="autoapp", description=help_text, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("command", type=str, help="command", choices=['install', 'update', 'check'])
     args = parser.parse_args()
 
@@ -31,71 +37,38 @@ def main():
         apps: list[dict] = data['apps']
 
     # process command
-    match args.command:
-        case 'install':
-            install_app(apps)
-        case 'update':
-            update_app(apps)
-        case 'check':
-            check_app(apps)
+    process_command(args.command, apps)
 
 
-def install_app(apps: list[dict]):
-    """ Install applications using `winget` or manually. """
-
+def process_command(command: str, apps: list[dict]):
     for i, app in enumerate(apps, start=1):
         print(COLOR_START + f"({i}/{len(apps)}) Start install {app['name']}...")
 
-        match app['method']:
-            case 'winget':
+        match (command, app['method']):
+            case ('install' | 'check', 'winget'):
                 result = subprocess.run(['winget', 'list', app['id']], capture_output=True, text=True, encoding='utf-8')
                 if app['id'] in result.stdout:
                     print(COLOR_INFO + f"{app['name']} is already installed.")
-                else:
+                elif command == 'install':
                     os.system(f'winget install --id {app['id']} --source winget')
+                elif command == 'check':
+                    print(COLOR_ERROR + f"{app['name']} is not installed.")
 
-            case 'manual':
+            case ('update', 'winget'):
+                os.system(f'winget upgrade --id {app['id']}')
+
+            case ('install' | 'update', 'manual'):
                 webbrowser.open(app['url'])
-                input(COLOR_INFO + f"Please install {app['name']} manually.")
+                input(COLOR_INFO + f"Please {command} {app['name']} manually.")
+
+            case ('check', 'manual'):
+                os.system(f'winget search "{app['name']}"')
+                print(COLOR_INFO + f"Please check {app['name']} manually.")
 
             case _:
                 print(COLOR_ERROR + "Error: Wrong method.")
 
-        print(COLOR_FINISH + f"Finish install {app['name']}.\n")
-
-
-def update_app(apps: list[dict]):
-    """ Update applications using `winget` or manually. """
-
-    for i, app in enumerate(apps, start=1):
-        print(COLOR_START + f"({i}/{len(apps)}) Start update {app['name']}...")
-
-        if app['method'] == 'winget':
-            os.system(f'winget upgrade --id {app['id']}')
-        else:
-            os.system(f'winget search "{app['name']}"')
-            input(COLOR_INFO + f"Please update {app['name']} manually.")
-
-        print(COLOR_FINISH + f"Finish update {app['name']}.\n")
-
-
-def check_app(apps: list[dict]):
-    """ Check if applications are installed or available via `winget`. """
-
-    for i, app in enumerate(apps, start=1):
-        print(COLOR_START + f"({i}/{len(apps)}) Start check {app['name']}...")
-
-        if app['method'] == 'winget':
-            result = subprocess.run(['winget', 'list', app['id']], capture_output=True, text=True, encoding='utf-8')
-            if app['id'] in result.stdout:
-                print(COLOR_INFO + f"{app['name']} is already installed.")
-            else:
-                print(COLOR_ERROR + f"{app['name']} is not installed.")
-        else:
-            os.system(f'winget search "{app['name']}"')
-            print(COLOR_INFO + f"Please check {app['name']} manually.")
-
-        print(COLOR_FINISH + f"Finish check {app['name']}.\n")
+        print(COLOR_FINISH + f"Finish {command} {app['name']}.\n")
 
 
 if __name__ == '__main__':
